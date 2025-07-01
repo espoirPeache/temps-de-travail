1e95ceea6eaa4a22f8ec6db6e45332ee96db7218
Espoir Peach <ew763842@gmail.com>
2025-07-01 17:10:43 +0100

    ajout de package lottie pour le design -un background anime en aleatoire a chaque demarage

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,12 +1,18 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:gestion_de_temps/settings.dart';
 import 'package:gestion_de_temps/timerModel.dart';
 import 'package:percent_indicator/percent_indicator.dart';
+import 'package:lottie/lottie.dart';
 
 import 'timer.dart';
 import 'widget.dart';
 
 class Homepage extends StatefulWidget {
+
+  late final String selectedBackground;
+
    Homepage({super.key});
 
   @override
@@ -14,8 +20,23 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+
   final paddingDefault = 8.0;
   final CountTimer timer = CountTimer();
+
+  final List<String> backgrounds = [
+    'assets/animations/background1.json',
+    'assets/animations/background2.json',
+    'assets/animations/background3.json',
+  ];
+  late String selectedBackground;
+
+  @override
+  void initState() {
+    super.initState();
+    final random = Random();
+    selectedBackground = backgrounds[random.nextInt(backgrounds.length)];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,104 +80,111 @@ class _HomepageState extends State<Homepage> {
           )
         ],
       ),
-      body: LayoutBuilder(
-        builder: (context, BoxConstraints constraints) {
-          final double width = constraints.maxWidth;
-          return Column(
-            children: [
-              Row(
+      body: Stack(
+        children:[
+          Positioned.fill(
+            child: Lottie.asset(selectedBackground, fit: BoxFit.cover),
+          ),
+          LayoutBuilder(
+            builder: (context, BoxConstraints constraints) {
+              final double width = constraints.maxWidth;
+              return Column(
                 children: [
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.all(paddingDefault),
-                      child: Button(color: Colors.blueAccent, size: 0, text: "Travail", onPresed:()=> timer.startWork(),),
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.all(paddingDefault),
-                      child: Button(color: Colors.lightBlueAccent, size: 0, text: "Pause courte", onPresed:()=>timer.startBreak(true),),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.all(paddingDefault),
+                          child: Button(color: Colors.blueAccent, size: 0, text: "Travail", onPresed:()=> timer.startWork(),),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.all(paddingDefault),
+                          child: Button(color: Colors.lightBlueAccent, size: 0, text: "Pause courte", onPresed:()=>timer.startBreak(true),),
 
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.all(paddingDefault),
-                      child: Button(color: Colors.blueGrey, size: 0, text: "Pause longue", onPresed:()=>timer.startBreak(false)),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.all(paddingDefault),
+                          child: Button(color: Colors.blueGrey, size: 0, text: "Pause longue", onPresed:()=>timer.startBreak(false)),
 
-                    ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              StreamBuilder(
-                  stream: timer.stream(),
-                  initialData: "00:00",
-                  builder: (context, asyncSnapshot) {
-                    TimerModel timer = (asyncSnapshot.data == "00:00") ? TimerModel("00:00", 1): asyncSnapshot.data as TimerModel;
-                    return Expanded(
-                        child: CircularPercentIndicator(
-                          radius: width / 2.3,
-                          lineWidth: 30,
-                          animation: true,
-                          animateFromLastPercent: true,
-                          percent: timer.percent,
-                          reverse: true,  // de droite vers la gauche
-                          backgroundColor: Colors.grey.shade900,
-                          circularStrokeCap: CircularStrokeCap.round, // bouts arrondis
-                          linearGradient: LinearGradient(
-                            colors: [
-                              Colors.cyanAccent,
-                              Colors.blueAccent,
-                              Colors.deepPurpleAccent,
-                            ],
-                          ),
-                          center: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.black.withOpacity(0.6),
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            padding: EdgeInsets.all(12),
-                            child: Text(
-                              timer.time,
-                              style: TextStyle(
-                                fontSize: 36,
-                                fontFamily: 'Digital', // police numérique sympa
-                                fontWeight: FontWeight.bold,
-                                color: Colors.cyanAccent,
-                                shadows: [
-                                  Shadow(
-                                    blurRadius: 10,
-                                    color: Colors.cyanAccent.withOpacity(0.7),
-                                    offset: Offset(0, 0),
-                                  )
+                  StreamBuilder(
+                      stream: timer.stream(),
+                      initialData: "00:00",
+                      builder: (context, asyncSnapshot) {
+                        TimerModel timer = (asyncSnapshot.data == "00:00") ? TimerModel("00:00", 1): asyncSnapshot.data as TimerModel;
+                        return Expanded(
+                            child: CircularPercentIndicator(
+                              radius: width / 2.3,
+                              lineWidth: 30,
+                              animation: true,
+                              animateFromLastPercent: true,
+                              percent: timer.percent,
+                              reverse: true,  // de droite vers la gauche
+                              backgroundColor: Colors.grey.shade900,
+                              circularStrokeCap: CircularStrokeCap.round, // bouts arrondis
+                              linearGradient: LinearGradient(
+                                colors: [
+                                  Colors.cyanAccent,
+                                  Colors.blueAccent,
+                                  Colors.deepPurpleAccent,
                                 ],
                               ),
-                            ),
-                          ),
-                        )
+                              center: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.black.withOpacity(0.6),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                padding: EdgeInsets.all(12),
+                                child: Text(
+                                  timer.time,
+                                  style: TextStyle(
+                                    fontSize: 36,
+                                    fontFamily: 'Digital', // police numérique sympa
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.cyanAccent,
+                                    shadows: [
+                                      Shadow(
+                                        blurRadius: 10,
+                                        color: Colors.cyanAccent.withOpacity(0.7),
+                                        offset: Offset(0, 0),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            )
 
-                    );
-                  }
-              ),
-              Row(
-                children: [
-                  Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.all(paddingDefault),
-                        child: Button(size: 0, color: Colors.red, text: "stop", onPresed:()=> timer.stopTimer()),
-                      )
+                        );
+                      }
                   ),
-                  Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.all(paddingDefault),
-                        child: Button(size: 0, color: Colors.green, text: "start", onPresed:()=> timer.startTimer()),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: Padding(
+                            padding: EdgeInsets.all(paddingDefault),
+                            child: Button(size: 0, color: Colors.red, text: "stop", onPresed:()=> timer.stopTimer()),
+                          )
+                      ),
+                      Expanded(
+                          child: Padding(
+                            padding: EdgeInsets.all(paddingDefault),
+                            child: Button(size: 0, color: Colors.green, text: "start", onPresed:()=> timer.startTimer()),
+                          )
                       )
+                    ],
                   )
                 ],
-              )
-            ],
-          );
-        },
+              );
+            },
+          ),
+        ]
       ),
     );
   }
